1ea8e18ecd2533ec3e4ac78f04a96bb39b325247
Matthew Shockey <mattshockey@yahoo.com>
2016-04-04 15:58:46 -0500

    removed tagline, inserted logo, full width nav

--- wp-content/themes/storefront/inc/woocommerce/hooks.php
+++ wp-content/themes/storefront/inc/woocommerce/hooks.php
@@ -57,14 +57,6 @@ remove_action( 'woocommerce_before_shop_loop_item_title', 	'woocommerce_show_pro
 add_action( 'woocommerce_after_shop_loop_item_title', 		'woocommerce_show_product_loop_sale_flash', 6 );
 
 /**
- * Header
- * @see  storefront_product_search()
- * @see  storefront_header_cart()
- */
-add_action( 'storefront_header', 'storefront_product_search', 	40 );
-add_action( 'storefront_header', 'storefront_header_cart', 		60 );
-
-/**
  * Filters
  * @see  storefront_woocommerce_body_class()
  * @see  storefront_cart_link_fragment()
